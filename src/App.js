3f48db5cb4b6d89d7b894a0ff0cd93683c7717da
CMSangam <148764643+CMSangam@users.noreply.github.com>
2023-10-23 20:55:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ function App() {
         <h5>Created this website to impress the most beautiful girl I saw sipping her sweet and sour beer on a chaotic friday evening!</h5>
         <h2>Click the below Image for a small surprise</h2>
         <ImageBlender />
-        <h4>Disclaimer: You will be dealing with a guy who will make too many stupid jokes</h4>
-        <h5>You have all the rights to like me if you start loving my jokes</h5>
-        <h6>Haven't beautified the site yet, As far as my brain understands, you will need to help me decide the colour comnbinations!</h6>
+        <div>
+        <h4>Disclaimer: You will be dealing with a guy who will make too many stupid jokes :D</h4>
+        <h5>You have all the rights to like me if you start loving my jokes though... </h5>
+        <h5>Haven't beautified the site yet. As far as my brain understands, you will need to help me decide the colour combinations!</h5>
+        </div> 
       </header>
     </div>
   );
